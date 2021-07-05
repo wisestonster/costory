afe1a996a04be85588b709c4c385546e3dc85ea5
DESKTOP-2B0SMLO\RB <wisestonster@gmail.com>
2021-07-05 17:53:01 +0900

    forms 적용

---
+++ posts/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class PostForm(forms.Form):
+    title = forms.CharField(max_length=50, label='제목')
+    content = forms.CharField(label='내용', widget=forms.Textarea)
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 def post_list(request):
@@ -11,3 +12,7 @@ def post_detail(request, post_id):
     post = Post.objects.get(id=post_id)
     context = {"post": post}
     return render(request, 'posts/post_detail.html', context)
+
+def post_create(request):
+    post_form = PostForm()
+    return render(request, 'posts/post_form.html', {'form': post_form})
