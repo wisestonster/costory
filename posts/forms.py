ee0c302fa30251abf67af46ff7c6c8364f49c6a2
DESKTOP-F72FJ8C\wises <wisestonster@gmail.com>
2021-07-06 21:35:07 +0900

    CSS 적용

--- posts/forms.py
+++ posts/forms.py
@@ -1,7 +1,25 @@
 from django import forms
 from .models import Post
+from .validators import validate_symbols
+from django.core.exceptions import ValidationError
 
 class PostForm(forms.ModelForm):
+
     class Meta:
         model = Post
         fields = ['title', 'content']
+        # 기본 widget 대신 다른 widget 사용
+        widgets = {
+            'title': forms.TextInput(attrs={
+                "class": 'title',
+                "placeholder": '제목을 입력 하세요.'}),
+            'content': forms.Textarea(attrs={
+                'placeholder': '내용을 입력 하세요.'})
+        }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if '*' in title:
+            raise ValidationError('*는 포함될 수 없습니다.')
+
+        return title
